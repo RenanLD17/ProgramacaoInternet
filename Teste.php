c4f4ff479520fba7b3ae6f5848ae42b050874d0c
RenanLD17 <110139382+RenanLD17@users.noreply.github.com>
2024-09-08 21:05:44 -0300

    Add files via upload

---
+++ Teste.php
@@ -0,0 +1,11 @@
+<?php
+        // Exibindo uma mensagem
+        echo "<p>Olá, este é um teste de PHP!</p>";
+
+        // Fazendo uma operação de soma
+        $num1 = 10;
+        $num2 = 20;
+        $soma = $num1 + $num2;
+
+        // Exibindo o resultado da soma
+        echo "<p>A soma de $num1 e $num2 é $soma.</p>";
